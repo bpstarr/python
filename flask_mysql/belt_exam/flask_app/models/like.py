fca9152d61cc0c30dda83ad82b2806549be29a66
bpstarr <87786124+bpstarr@users.noreply.github.com>
2021-09-21 19:46:09 -0500

    Delete flask_mysql/belt_exam directory

--- flask_mysql/belt_exam/flask_app/models/like.py
+++
@@ -1,8 +0,0 @@
-from flask_app import app
-from flask_app.config.mysqlconnection import connectToMySQL
-class Like():
-    def __init__(self,data):
-        self.id = data['id']
-        self.likes = data['likes']
-        self.user_id = data['user_id']
-        self.thought_id = data['thought_id']
